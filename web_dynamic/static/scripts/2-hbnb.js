404f01e9092cec66a6286a943b495092b0d63916
Ruby <graceffiong@gmail.com>
2022-10-30 22:56:39 +0100

    correct js code

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 $(
     function(){
-        const request = require('request');
-        request.get('http://0.0.0.0:5001/api/v1/status/').on('response', function(response){
-            if(response.statusCode === 200){
+        $.get('http://0.0.0.0:5001/api/v1/status/', function(data, response){
+            $('div#api_status').text(response);
+            if(response === 'OK'){
                 $('div#api_status').addClass('available');
-                console.log('hi');
             } else {
                 $('div#api_status').removeClass('available');
             }
-        })
+    });
     }
 );
