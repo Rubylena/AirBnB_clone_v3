a352f15e4c0bc989d297d1f3b631aef8a2b57ddc
Ruby <graceffiong@gmail.com>
2022-10-30 22:16:39 +0100

    task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,12 @@
+$(
+    function(){
+        // const request = require ('request');
+        $.get('http://0.0.0.0:5001/api/v1/status/').on('response', function(response){
+            if(response.statusCode === 200){
+                $('div#api_status').addClass('available');
+            } else {
+                $('div#api_status').removeClass('available');
+            }
+        })
+    }
+);
